e7454a6e13c9514c7ea839999ba77d7e5bd2943f
Your Namesimcovvlad <vladislav.simcov.studenti@isii.it>
2023-04-14 08:49:17 +0200

    inizio

---
+++ db_dal_inc.php
@@ -0,0 +1,19 @@
+<?php  
+require('config.php');
+
+function db_connect(){
+    global $servername, $username, $password, $dbname;
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    return $conn;
+}
+function selectAllVisite($conn)
+{
+  $sql="SELECT idV,tipoV,data,ora,descrizionefrom studente order by idV ";
+  $result=$conn->query($sql);
+  $rowsStudenti=$result->fetch_all(MYSQLI_ASSOC);
+  return$rows;
+}
+?>
